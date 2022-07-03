7e2b811959b22ce820cdf346cd19721397d574ca
kartiksoni1002 <106035727+kartiksoni1002@users.noreply.github.com>
2022-07-03 19:41:47 -0400

    2 screens

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Signup from './components/Signup/Signup';
-import Login from './components/Login/Login';
+import React from 'react';
+import './App.css'
+import { Route,Routes, Switch } from 'react-router-dom';
+import RegLog from './components/LogReg/RegLog';
+import Home from './components/Home/Home';
 
-function App() {
+const App = () => {
   return (
-    <>
-
-      <Login/>
-      <Signup />
-
-    </>
-
+    <div>
+        <Routes>
+          <Route exact  path='/' element={<RegLog/>}/>
+          <Route exact path='/Home' element={<Home/>} />
+        </Routes>
+    </div>
   );
-}
+};
 
 export default App;
