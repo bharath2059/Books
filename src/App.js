876bc1e39014f7efd9209e5bd1a5cff38299df59
sandipyadav4799 <105245534+sandipyadav4799@users.noreply.github.com>
2022-07-03 01:46:49 -0400

    Login/Signup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Signup from './components/Signup/Signup';
+import Login from './components/Login/Login';
+
+function App() {
+  return (
+    <>
+
+<Login/>
+
+    </>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Signup/Signup.jsx
@@ -0,0 +1,21 @@
+import "./Signup.css";
+
+const Signup = () => {
+    return (
+        <div className="signup" >
+            <h2>Create An Account</h2>
+            <form>
+                <input type={'email'} placeholder={'Email'} />
+                <input type={'password'} placeholder={'password'} />
+                <input type={'password'} placeholder={'Confirm password'} />
+                <button type={'submit'}>SignUp</button>
+            </form>
+            <br />
+            <div>
+                <a href="#">Already have an account?</a>
+            </div>
+        </div >
+    );
+};
+
+export default Signup;
