e9ee8e2d30819b2bbe2b43e86a43ed1603559b2c
Sandip Yadav <sandipyadav@Sandips-MacBook-Air.local>
2022-07-06 15:55:14 -0400

    LoginPage

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ export default function App() {
     <SafeAreaView style =  {styles.AndroidSafeArea}>
     
      
-      <CreateSignupPage/>
+      <CreateLoginPage/>
       
 
       
--- src/screens/loginpage.tsx
+++ src/screens/loginpage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useState } from "react";
-import { SafeAreaView, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, TouchableOpacity, Alert,Image } from 'react-native';
 
 const CreateLoginPage = () => {
     const [Email, setEmail] = useState('')
     const [Password, setPassword] = useState('')
     return (
         <SafeAreaView style={styles.Wrapper}>
+            <Image source={require('./../Assets/logo.jpeg')} style={styles.imageStyle} />
             <View style={styles.box1}>
                 <Text style={styles.HeadingText}>Login</Text>
                 
@@ -36,7 +37,7 @@ const CreateLoginPage = () => {
 const styles = StyleSheet.create({
     Wrapper: {
         flex: 1,
-        paddingTop:170,
+        paddingTop:80,
         backgroundColor: '#E8E8FF',
     },
     HeadingText: {
@@ -44,10 +45,19 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textAlign: 'center'
     },
+    
+    imageStyle: {
+        height: 200,
+        width: 200,
+        left: 105,
+        bottom: 40,
+        alignItems:'center',
+    },
 
     box1: {
         flex: 3,
     },
+
     link:{
         marginTop:40,
         fontSize: 20,
@@ -69,7 +79,7 @@ const styles = StyleSheet.create({
         width: 250,
         backgroundColor: '#D9D9D9',
         marginBottom: 0,
-        marginTop:100,
+        marginTop:80,
         marginLeft:80,
         borderRadius:10
 
